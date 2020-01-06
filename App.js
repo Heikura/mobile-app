d08ab68af125a567bc80f40b3f54b16777d9a177
Artoiss <aarttu@windowslive.com>
2020-01-06 12:34:27 +0200

    Background centered and buttons modified.

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
 import { ImageBackground } from 'react-native';
 
+function Separator() {
+  return <View style={styles.separator} />;
+}
+
 export default function App() {
   return (
-    <ImageBackground source={require("./images/background3.jpg")} style={styles.container}>
-    <View style={styles.container}>
-
-      <Button type='solid' title="New exercise" />
+    <ImageBackground source={require("./images/background.png")} style={styles.container}>
+    <View style={styles.buttonContainer}>
+      <Button type='solid' title="New exercise"  />
+    <Separator />
       <Button type='outline' title="Inspect exercises" />
     </View>
     </ImageBackground>
@@ -21,5 +25,18 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  buttonContainer: {
+    flex: 0,
+    height: 20,
+    padding: 5,
+    position: 'absolute',
+    bottom:250
+  },
+
+  separator: {
+    marginVertical: 8,
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
