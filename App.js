9f191f73231c658272af4f292900955f142cb148
Artoiss <aarttu@windowslive.com>
2020-01-06 12:04:02 +0200

    Background image

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native-elements';
+import { ImageBackground } from 'react-native';
 
 export default function App() {
   return (
+    <ImageBackground source={require("./images/background3.jpg")} style={styles.container}>
     <View style={styles.container}>
+
       <Button type='solid' title="New exercise" />
       <Button type='outline' title="Inspect exercises" />
     </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'transparent',
     alignItems: 'center',
     justifyContent: 'center',
   },
